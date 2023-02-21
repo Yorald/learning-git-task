e47ed121f4607b7c97aded9fce17174e31301655
Yorald <maksym.miazek@gmail.com>
2023-02-21 19:06:28 +0100

    Add for function

--- learning-git-task.py
+++ learning-git-task.py
@@ -2,3 +2,5 @@ shopping_list = {
     "piekarnia": ["chleb", "bułki", "paczek"],
     "warzywniak": ["marchew", "seler", "rukola"]  
 }
+for shop, items in shopping_list.items():
+    print(f"Idę do {shop.capitalize()} i kupuje tam {items}")
