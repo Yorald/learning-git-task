8d88cdc55bf62c1d5b788e7ff4273cd079fe1166
Yorald <maksym.miazek@gmail.com>
2023-02-21 19:08:09 +0100

    Add total sum of products bought

--- learning-git-task.py
+++ learning-git-task.py
@@ -4,3 +4,8 @@ shopping_list = {
 }
 for shop, items in shopping_list.items():
     print(f"Idę do {shop.capitalize()} i kupuje tam {items}")
+x=0
+for shop, items in shopping_list.items():
+    x=x + len(items)
+
+print(f"W sumie kupuje {x} produktów")    
